49b719b1dffc2a8682fa1bf62a770df54c5b4748
XJ <1570884715@qq.com>
2022-11-03 15:29:02 +0800

    feat: 配置数据

--- src/main/config.tsx
+++ src/main/config.tsx
@@ -1,3 +1,10 @@
+export const adminAvatar =
+  "https://img0.baidu.com/it/u=3400779666,996797193&fm=253&fmt=auto&app=138&f=JPEG?w=518&h=500";
+export const navBar = {
+  logo: adminAvatar,
+  title: "摸摸猫爪咨询中心",
+  position: "left",
+};
 export const initialMessages = [
   {
     type: "system",
@@ -9,19 +16,23 @@ export const initialMessages = [
     type: "text",
     content: { text: "主人好，我是智能助理，你的贴心小助手~" },
     user: {
-      avatar:
-        "	https://img1.baidu.com/it/u=1394839373,2163428572&fm=253&fmt=auto&app=138&f=JPEG?w=374&h=374",
+      avatar: adminAvatar,
     },
   },
   {
     type: "card",
     content: {
-      picUrl:
-        "https://gw.alicdn.com/tfs/TB1HURhcBCw3KVjSZR0XXbcUpXa-750-364.png",
+      code: "knowledge",
+      data: {
+        text: "内容（支持富文本）",
+      },
+    },
+    meta: {
+      evaluable: true, // 是否展示点赞点踩按钮
     },
   },
 ];
-export const defaultQuickReplies = [
+export const quickReplies = [
   {
     icon: "message",
     name: "联系人工服务",
@@ -29,11 +40,33 @@ export const defaultQuickReplies = [
     isHighlight: true,
   },
   {
-    name: "缴费",
+    name: "吸猫多少钱",
     isNew: true,
   },
   {
-    name: "没有网络",
-    isHighlight: true,
+    name: "有哪些猫可以选择",
+    type: "url",
+    url: "https://www.taobao.com/",
+  },
+];
+export const toolbar = [
+  {
+    type: "image",
+    icon: "image",
+    title: "相册",
+  },
+  {
+    type: "speech",
+    icon: "mic",
+    title: "语音输入",
+  },
+];
+export const sidebar = [
+  {
+    title: "公告",
+    code: "richtext",
+    data: {
+      text: '<p>这里是富文本内容，支持<a href="https://chatui.io/sdk/getting-started">链接</a>，可展示图片<img src="https://gw.alicdn.com/tfs/TB17TaySSzqK1RjSZFHXXb3CpXa-80-80.svg" /></p>',
+    },
   },
 ];
